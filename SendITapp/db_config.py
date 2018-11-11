8aed6b72a09d961678efbb1fb8e0df5131516337
Partck <patgachuru@gmail.com>
2018-11-11 21:47:43 +0300

    Update fixes

--- SendITapp/db_config.py
+++ SendITapp/db_config.py
@@ -10,14 +10,30 @@ port = 5432
 url = "host ={} user = {}, pasword, port"
 con = psycopg2.connect(url)
 cur = con.cursor()
-sql = ""
 cursor.execute(sql)
 
 
+def connection(url):
+    con = psycopg2.connect(url)
+    return con
 
-# Has methods
-# connection
-# init_db
-# create_tables
-# destroy_tables
-# tables
+
+def init_db():
+    con = connection(url)
+    return con
+
+def create_tables():
+    conn = connection(url)
+    cursor = conn.cursor()
+    queries = tables()
+    for query in queries:
+        cursor.execute(query)
+    conn.commit()
+
+
+def destroy_tables():
+    pass
+
+
+def tables():
+    pass
