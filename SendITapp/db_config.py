75116d0357072924aa7744e9037009eec493172b
Partck <patgachuru@gmail.com>
2018-11-20 18:07:40 +0300

    [Finishes #161918474] Fixing db

--- SendITapp/db_config.py
+++ SendITapp/db_config.py
@@ -1,11 +1,10 @@
 import os
 import psycopg2
-"""
+
 url = "dbname = 'd8osr4jq6ahd25' host = 'ec2-54-204-36-249.compute-1.amazonaws.com' port = '5432'\
      user = 'jkaegobpsrhntk' password = 'ee912001d1be919a6e88385a69e75f54f4c757ca9ca5293ea25e28b29d267148'"
-"""
-url = "dbname = 'senditdb' host = 'localhost' port = '5432'\
-             user = 'sendit_user' password = 'qwerty'"
+
+
 def connection(url):
     con = psycopg2.connect(url)
     return con
