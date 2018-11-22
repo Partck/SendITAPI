f1939ecde17482ec1e399eb13a146fc132ac6824
Partck <patgachuru@gmail.com>
2018-11-22 12:56:42 +0300

    [Finishes #162108060] Added version TWO user views and models

--- SendITapp/api/v2/__init__.py
+++ SendITapp/api/v2/__init__.py
@@ -1,9 +1,9 @@
 """From flask import Blueprint. Name it : api_v1."""
 from flask import Blueprint
 from SendITapp.api.v2.views.parcelview import AllParcels, SingleParcel,\
-    ParcelsByUser, CancelParcelOrder, UpdateOrder
+    ParcelsByUser, CancelParcelOrder, UpdateOrder, UpdateDestinationOrder
 from flask_restful import Api
-from SendITapp.api.v2.views.userView import Register, SingleUser, Login
+from SendITapp.api.v2.views.userView import Register, SingleUser, Login, AllUsers
 
 superv2_blueprint = Blueprint("api_v2", __name__, url_prefix='/api/v2')
 
@@ -13,7 +13,9 @@ blueprint_api.add_resource(AllParcels, "/parcels")
 blueprint_api.add_resource(SingleParcel, "/parcels/<parcelid>")
 blueprint_api.add_resource(Register, "/register")
 blueprint_api.add_resource(SingleUser, "/users/<userid>")
+blueprint_api.add_resource(AllUsers, "/users")
 blueprint_api.add_resource(ParcelsByUser, "/users/<userid>/parcels")
 blueprint_api.add_resource(CancelParcelOrder, "/parcels/<parcelid>/cancel")
+blueprint_api.add_resource(UpdateDestinationOrder, "/parcels/<parcelid>/update_destination")
 blueprint_api.add_resource(UpdateOrder, "/parcels/<parcelid>/update")
 blueprint_api.add_resource(Login, "/login")
--- SendITapp/api/v2/views/userView.py
+++ SendITapp/api/v2/views/userView.py
@@ -1,66 +1,58 @@
 
 from flask import request, make_response, jsonify
+from werkzeug.security import check_password_hash
 import re
-from flask_jwt_extended import create_access_token
 from flask_restful import Resource
 from SendITapp.api.v2.models.users import UserModel
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 import bcrypt
 
 
 class Register(Resource):
     """Handle user CRUD."""
 
-   
-
     def post(self):
         """Create user."""
         data = request.get_json() or {}
         user = UserModel()
 
-
-        username = data['username'].strip()
-        name = data['name'].strip()
-        if not (re.match("^[a-zA-Z0-9_]*$", username)) or\
-            not (re.match("^[a-zA-Z0-9_]*$", name)):
-            message = 'Check your name and usename'
+        if data['username'] == "":
+            message = 'Check your username'
             payload = {"Status": "Failed", "Message": message}
             return make_response(jsonify(payload), 400)
-            
 
-    
+        if data['name'] == "":
+            message = 'Enter your name'
+            payload = {"Status": "Failed", "Message": message}
+            return make_response(jsonify(payload), 400)
+             
         if data["email"].find("@") < 2:
             message = 'Incorrect email format'
             payload = {"Status": "Failed", "Message": message}
             return make_response(jsonify(payload), 400)
-            
-
-    
-        password = data['password'].strip()
-        if not (re.match("^[a-zA-Z0-9_]*$", password)) or \
-        data['password'] != data["retype_password"]:
-            message = 'Check your password.'
+                
+        if data['password'] == "":
+            message = 'Enter your password.'
             payload = {"Status": "Failed", "Message": message}
             return make_response(jsonify(payload), 400)
-            
 
-    
+        if data['password'] != data["retype_password"]:
+            message = 'Passwords do not match.'
+            payload = {"Status": "Failed", "Message": message}
+            return make_response(jsonify(payload), 400)
+                
         if data['role'] != "Admin" and data["role"] != "User":
             message = 'Role input can either be Admin or User'
             payload = {"Status": "Failed", "Message": message}
             return make_response(jsonify(payload), 400)
-            
-
-    
+                
         if data['phone'] == "":
             message = 'Enter a valid number.'
             payload = {"Status": "Failed", "Message": message}
             return make_response(jsonify(payload), 400)
             
-
-        password = bcrypt.hashpw(data["password"], bcrypt.gensalt())
-        reply_info = user.create_user(data["username"], data["name"],
-         data["email"], data["role"], data["phone"], password)
+        # password = bcrypt.hashpw(data["password"], bcrypt.gensalt())
+        reply_info = user.create_user(data)
 
         if reply_info:
             user_data = [data["username"], data["name"],
@@ -68,18 +60,17 @@ class Register(Resource):
 
             payload = {"Status": "created",
             "User": user_data}
-            return make_response(jsonify(payload), 200)
-            
-
+            return make_response(jsonify(payload), 200) 
         payload = {"Status": "Failed", "Message": reply_info}
         return make_response(jsonify(payload), 400)
         
-
+class AllUsers(Resource):
     @jwt_required
     def get(self):
         """Method to get all the parcels."""
-        user1 = UserModel()
-        all_users = user1.get_all()
+        
+        user = UserModel()
+        all_users = user.get_all()
 
         payload = {
             "Status": "Ok",
@@ -87,8 +78,6 @@ class Register(Resource):
         }
         return make_response(jsonify(payload), 200)
         
-
-
 class SingleUser(Resource):
     """Single parcel class."""
     @jwt_required
@@ -114,34 +103,37 @@ class Login(Resource):
 
     def post(self):
         data = request.get_json() or {}
-        
+
+        if data["email"] == "":
+            message = 'Enter email'
+            payload = {"Status": "Failed", "Message": message}
+            return make_response(jsonify(payload), 400)
+
         if data["email"].find("@") < 2:
             message = 'Incorrect email format'
             payload = {"Status": "Failed", "Message": message}
             return make_response(jsonify(payload), 400)
-            
+        
+        if data["password"] == "":
+            message = 'Enter your password.'
+            payload = {"Status": "Failed", "Message": message}
+            return make_response(jsonify(payload), 400)
 
-    
         password = data['password'].strip()
         if not (re.match("^[a-zA-Z0-9_]*$", password)):
             message = 'Check your password.'
             payload = {"Status": "Failed", "Message": message}
             return make_response(jsonify(payload), 400)
-            
-        
+                    
         user = UserModel()
-        email = data["email"]
-        password = data["password"]
-        auth = user.user_login(email)
-        
-        if auth:
-            access_token = create_access_token(identity=email)
+        password = user.user_password(data["email"])
+        email = user.user_email(data["email"])
+
+        if check_password_hash(password["password"], data["password"]):
+            access_token = create_access_token(identity=email["email"])
             return make_response(jsonify({"message": "Successful login",
             "token": access_token
         }), 200)
         return make_response(jsonify({
         "message": "Try again",
         }), 400)
-
-        
-
