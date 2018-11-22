cf658f2b3da1ef681faeda17bc2918e64a6957ca
Partck <patgachuru@gmail.com>
2018-11-22 15:59:23 +0300

    [Finishes #162108060] Added Chellenge 3 Tests

---
+++ SendITapp/tests/v2/tests_data.py
@@ -0,0 +1,99 @@
+incorrect_sender = {
+            "parcelid": "1",
+            "destination": "Nairobi",
+            "recipient": "John",
+            "sender": "Mai}[na",
+            "weight": "24",
+            "price": "Paid",
+            "status": "Pending",
+            "userid": "2"}
+
+missing_sender = {
+            "parcelid": "1",
+            "destination": "Nairobi",
+            "recipient": "John",
+            "sender": "",
+            "weight": "24",
+            "price": "Paid",
+            "status": "Pending",
+            "userid": "2"}
+
+data = {
+            "parcelid": "1",
+            "destination": "Nairobi",
+            "recipient": "John",
+            "sender": "Maina",
+            "weight": "24",
+            "price": "Paid",
+            "status": "Pending",
+            "userid": "2"}
+
+missing_destination = {
+            "parcelid": "1",
+            "destination": "",
+            "recipient": "John",
+            "sender": "Maina",
+            "weight": "24",
+            "price": "Paid",
+            "status": "Pending",
+            "userid": "2"}
+
+incorrect_recipient = {
+            "parcelid": "1",
+            "destination": "Nairobi",
+            "recipient": "J#ohn",
+            "sender": "Maina",
+            "weight": "24",
+            "price": "Paid",
+            "status": "Pending",
+            "userid": "2"}
+missing_recipient = {
+            "parcelid": "1",
+            "destination": "Nairobi",
+            "recipient": "",
+            "sender": "Maina",
+            "weight": "24",
+            "price": "Paid",
+            "status": "Pending",
+            "userid": "2"}
+
+missing_weight = {
+            "parcelid": "1",
+            "destination": "Nairobi",
+            "recipient": "John",
+            "sender": "Maina",
+            "weight": "",
+            "price": "Paid",
+            "status": "Pending",
+            "userid": "2"}
+
+incorrect_weight = {
+            "parcelid": "1",
+            "destination": "Nairobi",
+            "recipient": "John",
+            "sender": "Maina",
+            "weight": "#",
+            "price": "Paid",
+            "status": "Pending",
+            "userid": "2"}
+
+incorrect_status = {
+            "parcelid": "1",
+            "destination": "Nairobi",
+            "recipient": "John",
+            "sender": "Maina",
+            "weight": "10",
+            "price": "dsadas",
+            "status": "Pending",
+            "userid": "2"}
+
+data_login = {
+"email": "patrick@mail.com",
+"password": "qwerty"
+}
+
+data_register = {
+                    "username": "",
+                    "name": "Pit Pat", "email": "patrick@mail.com", "role": "Admin",
+                    "phone": "0716904050", "password": "qwerty", "retype_password": "qwerty"
+ }
